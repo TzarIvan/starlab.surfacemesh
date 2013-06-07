0d2520d1c343d0e85b325da0a1870db6599adb6e
Andrea Tagliasacchi <andrea.tagliasacchi@gmail.com>
2013-06-07 21:47:07 +0200

    Controller draft

---
+++ mode_select/mode_select.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "StarlabMainWindow.h"
+#include "SurfaceMeshPlugins.h"
+
+/// FW
+class Octree;
+class ControllerManager;
+
+class mode_select : public ModePlugin{
+    Q_OBJECT
+    Q_INTERFACES(ModePlugin)
+
+    QIcon icon(){ return QIcon(":/icons/select_points.png"); }
+    bool isApplicable(){ return true; }
+
+private:
+    SurfaceMeshModel* mesh;
+    Octree* octree;
+    
+    ControllerManager* manager;
+    
+public:
+    /// Functions part of the EditPlugin system
+    void create();
+    void destroy();
+    void decorate();
+    void draw_with_names();
+    
+    bool mouseReleaseEvent(QMouseEvent*);
+
+
+//    bool mousePressEvent(QMouseEvent*);
+//    bool mouseMoveEvent(QMouseEvent*);
+};
---
+++ mode_select/mode_select.pro
@@ -0,0 +1,8 @@
+load($$[STARLAB])
+load($$[SURFACEMESH])
+load($$[OCTREE])
+StarlabTemplate(plugin)
+
+HEADERS += mode_select.h
+SOURCES += mode_select.cpp
+RESOURCES += mode_select.qrc
